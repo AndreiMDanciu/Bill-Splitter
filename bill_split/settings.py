98704cb356b939e8f4587cf1346fa4ee4c89909d
Andrei Danciu <danciuandreim@gmail.com>
2025-07-04 19:00:15 +0300

    Fix 3 render.yaml and settings.py for DEBUG

--- bill_split/settings.py
+++ bill_split/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-592ti8b3@%34-)w$ul=9(yuou3cb$91(7c)=w*bz)ewngww%#1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = 'FALSE'
 
 ALLOWED_HOSTS = ['*']
 
